9405d7960ef7c8a0c56cc7f876a4828616f88e02
GloriaAndonyaIT <gloriaandonyaa@gmail.com>
2025-04-28 20:58:02 +0300

    complete lab

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Oops! Looks like something went wrong.</h1>
+      <p>We couldn't find the page you were looking for.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
